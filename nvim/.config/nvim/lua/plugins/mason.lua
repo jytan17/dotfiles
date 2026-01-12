77db40eaa1eb02b154f935862c43935be5c486c9
Junyong Tan <jytan1701@gmail.com>
2026-01-12 05:09:17 +0000

    LSP Added to mason

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,7 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Mason
-
 ---@type LazySpec
 return {
   -- use mason-tool-installer for automatically installing Mason packages
@@ -13,15 +9,18 @@ return {
       ensure_installed = {
         -- install language servers
         "lua-language-server",
+        "basedpyright",
 
         -- install formatters
         "stylua",
+        "ruff",
 
         -- install debuggers
         "debugpy",
 
         -- install any other package
         "tree-sitter-cli",
+        "uv",
       },
     },
   },
