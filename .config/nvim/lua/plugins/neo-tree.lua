a2bbf86d8900a39e3e64c24ed4df4ab8118bd91c
Junyong Tan <junyongtan@Junyongs-MacBook-Pro.local>
2026-01-24 16:35:30 -0600

    made neo-tree the opening page

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -6,9 +6,21 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
+  event = "VimEnter",
   keys = {
     { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
-    { "<leader>o", "<cmd>Neotree focus<cr>", desc = "Focus Neo-tree" },
+    {
+      "<leader>o",
+      function()
+        local buf_name = vim.api.nvim_buf_get_name(0)
+        if buf_name:match("neo%-tree") then
+          vim.cmd("wincmd p")
+        else
+          vim.cmd("Neotree reveal")
+        end
+      end,
+      desc = "Toggle Neo-tree focus",
+    },
   },
   opts = {
     close_if_last_window = true,
@@ -30,6 +42,29 @@ return {
   },
   config = function(_, opts)
     require("neo-tree").setup(opts)
+
+    -- Track if we're still in "startup mode" (full screen neo-tree)
+    vim.g.neotree_startup_mode = true
+
+    -- Open neo-tree full screen on startup
+    vim.cmd("Neotree position=current")
+
+    -- When a file is opened, switch neo-tree to sidebar
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function()
+        if not vim.g.neotree_startup_mode then
+          return
+        end
+        local buf_name = vim.api.nvim_buf_get_name(0)
+        local buf_type = vim.bo.buftype
+        -- If entering a real file buffer (not neo-tree, not empty, not special)
+        if buf_type == "" and buf_name ~= "" and not buf_name:match("neo%-tree") then
+          vim.g.neotree_startup_mode = false
+          vim.cmd("Neotree close")
+          vim.cmd("Neotree show")
+        end
+      end,
+    })
 
     -- Refresh neo-tree when Neovim regains focus (e.g., after using lazygit in another tmux pane)
     vim.api.nvim_create_autocmd("FocusGained", {
