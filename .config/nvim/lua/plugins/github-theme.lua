a96fa742da09f9594df7a06ec5e6ed2ac57f3f40
Junyong Tan <junyongtan@Junyongs-MacBook-Pro.local>
2026-01-25 02:47:00 -0600

    changed directory color to blue

--- .config/nvim/lua/plugins/github-theme.lua
+++ .config/nvim/lua/plugins/github-theme.lua
@@ -2,7 +2,15 @@ return {
   "projekt0n/github-nvim-theme",
   priority = 1000,
   config = function()
-    require("github-theme").setup({})
+    require("github-theme").setup({
+      groups = {
+        github_dark_high_contrast = {
+          -- Neo-tree directory color
+          NeoTreeDirectoryIcon = { fg = "#91cbff" },  -- Constant blue
+          NeoTreeDirectoryName = { fg = "#91cbff" },  -- Constant blue
+        },
+      },
+    })
     vim.cmd.colorscheme("github_dark_high_contrast")
   end,
 }
