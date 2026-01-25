68b10b3ab6e5cac60ff294b08dad54726be98ada
Junyong Tan <junyongtan@Junyongs-MacBook-Pro.local>
2026-01-24 19:14:57 -0600

    Added telescope ui select for code actions

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-telescope/telescope-ui-select.nvim",
   },
   config = function()
     local telescope = require("telescope")
@@ -21,6 +22,7 @@ return {
     })
 
     telescope.load_extension("fzf")
+    telescope.load_extension("ui-select")
 
     -- keybindings
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
