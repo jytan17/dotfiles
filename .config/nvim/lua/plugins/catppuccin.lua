be1a82f52155a37216c6a948e8e44b433b67ecb5
Junyong Tan <junyongtan@Junyongs-MacBook-Pro.local>
2026-01-26 23:41:19 -0600

    adjusted background color of catp

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -6,12 +6,18 @@ return {
 		require("catppuccin").setup({
 			flavour = "mocha", -- latte, frappe, macchiato, mocha
 			term_colors = true,
+			color_overrides = {
+				mocha = {
+					base = "#15152a",
+				},
+			},
 			integrations = {
 				cmp = true,
 				gitsigns = true,
 				nvimtree = true,
 				treesitter = true,
 				notify = false,
+				todo_comments = true,
 				mini = {
 					enabled = true,
 					indentscope_color = "",
@@ -32,6 +38,16 @@ return {
 					DiagnosticUnderlineWarn = { undercurl = true },
 					DiagnosticUnderlineInfo = { undercurl = true },
 					DiagnosticUnderlineHint = { undercurl = true },
+					NeoTreeNormal = { bg = "#0d0d17" },
+					NeoTreeNormalNC = { bg = "#0d0d17" },
+
+					-- Make Sticky Context (Outline) more obvious
+					TreesitterContext = { bg = "#252540" },
+					TreesitterContextLineNumber = { bg = "#252540", fg = colors.rosewater },
+					TreesitterContextBottom = { sp = colors.rosewater, style = { "underline" } },
+
+					-- Make Indent Scope line brighter
+					MiniIndentscopeSymbol = { fg = colors.pink, style = { "bold" } },
 				}
 			end,
 		})
