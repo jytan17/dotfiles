e9257124f8cc004f0bd54a0fec4301743fa3b8ef
Junyong Tan <junyongtan@Junyongs-MacBook-Pro.local>
2026-01-27 01:18:46 -0600

    added git related configs and keymaps

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,78 @@
 return {
-  -- Git signs in the gutter
-  {
-    "lewis6991/gitsigns.nvim",
-    event = "VeryLazy",
-    opts = {},
-  },
+	-- Git signs in the gutter
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+		opts = {
+			signs = {
+				add = { text = "\u{258f}" }, -- ▎
+				change = { text = "\u{258f}" }, -- ▎
+				delete = { text = "\u{f0da}" }, -- 
+				topdelete = { text = "\u{f0da}" }, -- 
+				changedelete = { text = "\u{258f}" }, -- ▎
+				untracked = { text = "\u{258f}" }, -- ▎
+			},
+			current_line_blame = false, -- Show blame ghost text by default
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 200,
+				ignore_whitespace = false,
+			},
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, desc)
+					vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						return "]c"
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return "<Ignore>"
+				end, "Next Hunk")
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						return "[c"
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return "<Ignore>"
+				end, "Prev Hunk")
+
+				-- Actions (Git Group)
+				map("n", "<leader>gs", gs.stage_hunk, "Stage Hunk")
+				map("n", "<leader>gr", gs.reset_hunk, "Reset Hunk")
+				map("v", "<leader>gs", function()
+					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, "Stage Hunk (Visual)")
+				map("v", "<leader>gr", function()
+					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, "Reset Hunk (Visual)")
+				map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
+				map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
+				map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
+				map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
+				map("n", "<leader>gb", function()
+					gs.blame_line({ full = true })
+				end, "Blame Line")
+				map("n", "<leader>gd", gs.diffthis, "Diff This")
+				map("n", "<leader>gD", function()
+					gs.diffthis("~")
+				end, "Diff This ~")
+				map("n", "<leader>gq", "<cmd>diffoff! | only<cr>", "Quit Diff View")
+
+				-- Toggles (Toggle Group)
+				map("n", "<leader>tb", gs.toggle_current_line_blame, "Toggle git blame")
+				map("n", "<leader>td", gs.toggle_deleted, "Toggle deleted")
+			end,
+		},
+	},
 }
